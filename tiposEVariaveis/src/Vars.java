37183766a8f0a3111c980959681c578fdef40783
joao <joaohenrique131@gmail.com>
2024-04-30 19:52:49 -0300

    Alteraçoes com exemplos

--- tiposEVariaveis/src/Vars.java
+++ tiposEVariaveis/src/Vars.java
@@ -1,11 +1,22 @@
 public class vars {
-    public static void main(String[] args) throws Exception {
-[
-    
-]        double salarioMinimo = 2500;
+    public static void main(String [] args) throws Exception {
+            //Exemplos de variaveis
+
+            
+        double salarioMinimo = 2500;
 
         short numeroCurto = 1; 
         int numeroNormal = numeroCurto;
-        short numeroCurto2  = numeroNormal;
+        var numeroCurto2  = (short) numeroNormal;
+
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.print(numero);
+
+
+        final double valor_de_pi = 3.14;
     }
 }
