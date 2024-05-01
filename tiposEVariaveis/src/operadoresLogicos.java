647046c4b98eddbcd962fdfd78da4d66ad19ec55
joao <joaohenrique131@gmail.com>
2024-04-30 23:15:03 -0300

    Tipos e variaveis concluido

---
+++ tiposEVariaveis/src/operadoresLogicos.java
@@ -0,0 +1,15 @@
+public class operadoresLogicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && (7 > 4)) {
+            System.out.println("Ambas são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é Verdadeira");
+         }
+         System.out.println("Fim");
+        }
+
+}
--- tiposEVariaveis/src/relacionaisString.java
+++ tiposEVariaveis/src/relacionaisString.java
@@ -2,8 +2,10 @@ public class relacionaisString {
     public static void main(String[] args) {
         String nome1 = "Joao";
         String nome2 = "Joao";
+        //String nome2 = new String("Joao"); em OOP
         String resultado = "";
 
+        //System.out.println(nome1.equals(nome2)); em OOP
 
         /*if(nome1==nome2)
             resultado = "Verdadeiro";
