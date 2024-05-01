c8ff72c9c7072ebf0a601c37a56fe1e757604722
joao <joaohenrique131@gmail.com>
2024-04-30 22:13:30 -0300

    Commit

---
+++ tiposEVariaveis/src/ternario.java
@@ -0,0 +1,16 @@
+public class ternario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+        String resultado = "";
+        if (a==b)
+            resultado = "Verdadeiro";
+
+        else
+            resultado = "Falso";
+
+        System.out.println(resultado);
+    }
+}
---
+++ tiposEVariaveis/src/ternario2.java
@@ -0,0 +1,14 @@
+public class ternario2 {
+    public static void main (String [] args) {
+
+        int a = 5;
+        int b = 6;
+        
+        String resultado = (a==b) ? "Verdadeiro" : "Falso";
+
+
+        System.out.println(resultado);
+
+
+    }
+}
